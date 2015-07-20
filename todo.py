18d2997fc2be977cf92d821844764ae78acef0b0
rcjones <rcjones.iphone@gmail.com>
2015-07-19 19:57:21 -0500

    added new task route, function, and tmeplate

--- todo.py
+++ todo.py
@@ -1,15 +1,36 @@
 import sqlite3
-from bottle import route, run, debug, template
+from bottle import route, run, debug, template, request
 
 @route('/todo')
 def toto_list():
 	conn = sqlite3.connect('todo.db')
 	c = conn.cursor()
-	c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
+	#c.execute("SELECT id, task FROM todo WHERE status LIKE '0'")
+	c.execute("SELECT id, task FROM todo")
 	result = c.fetchall()
 	c.close()
 	output = template('make_table', rows=result)
 	return output
+
+@route('/new', method='GET')
+def new_item():
+
+    if request.GET.get('save','').strip():
+
+        new = request.GET.get('task', '').strip()
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
+        new_id = c.lastrowid
+
+        conn.commit()
+        c.close()
+
+        return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
+    else:
+        return template('new_task.tpl')
+
 	
 debug(True)
 run(reloader=True)
